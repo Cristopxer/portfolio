a7386022a945b7c2093767236059336929582984
cristopxer <cristopherortiz457@gmail.com>
2022-11-14 14:33:55 -0600

    Project setup/Navbar completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBarComponent from "./Components/Navbar/Navbar"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarComponent />
+    </div>
+  );
+}
+
+export default App;
